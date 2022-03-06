b56fa8ca5d18f9a3f70285061930fecee1c5fd0f
Chenhao Xue <xch927027@pku.edu.cn>
2022-03-06 19:01:15 +0800

    4x1 tiling v3

--- src/kernel/sgemm/v3.cu
+++ src/kernel/sgemm/v3.cu
@@ -1,16 +1,20 @@
 #include "kernel/sgemm.cuh"
 
 /**
+ * Notice that s_B(l, ty) could be reused for every thread with Idx y
+ * To reuse s_B(l,ty), we introduce a microkernel to compute a 4*1 tile
+ * For convenience, we only support matrix width divisible by `KERNEL_SIZE`
  * 
- * 
+ * Use float4 might accelerate a little bit, I leave it for future modification.
  */
 
 #define KERNEL_SIZE 32
+#define MICROKERNEL_SIZE 4
 #define DIV(i) (((i) + KERNEL_SIZE - 1) / KERNEL_SIZE)
 
-#define A(i,j) A[(i) + (j) * lda]
-#define B(i,j) B[(i) + (j) * ldb]
-#define C(i,j) C[(i) + (j) * ldc]
+#define A_(i,j) A_[(i) + (j) * lda]
+#define B_(i,j) B_[(i) + (j) * ldb]
+#define C_(i,j) C_[(i) + (j) * ldc]
 #define s_A(i,j) s_A[(i) + (j) * KERNEL_SIZE]
 #define s_B(i,j) s_B[(i) + (j) * KERNEL_SIZE]
 
@@ -23,37 +27,47 @@ void kernelFunc(int M, int N, int K, const float *alpha, const float *beta,
     int ldb = K;
     int ldc = M;
 
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-    int i = bx * KERNEL_SIZE + tx;
-    int j = by * KERNEL_SIZE + ty;
-    int k;
+    int tilex = threadIdx.x * MICROKERNEL_SIZE;
+    int tiley = threadIdx.y;
     int n = DIV(K);  // number of tiles on A and B
-    float sum = 0;
+    float sum0 = 0;
+    float sum1 = 0;
+    float sum2 = 0;
+    float sum3 = 0;
+    const float *A_ = A + blockIdx.x * KERNEL_SIZE;
+    const float *B_ = B + blockIdx.y * KERNEL_SIZE * K;
 
     __shared__ float s_A[KERNEL_SIZE*KERNEL_SIZE];
     __shared__ float s_B[KERNEL_SIZE*KERNEL_SIZE];
 
-    if (i >= M || j >= N)
-        return;
-
     for (int tile = 0; tile < n; tile++) {
-        // copy data to shared memory
-        k = tile * KERNEL_SIZE + ty;
-        s_A(tx, ty) = k < K ? A(i, k) : 0;
-
-        k = tile * KERNEL_SIZE + tx;
-        s_B(tx, ty) = k < K ? B(k, j) : 0;
-        __syncthreads();
-
-        for (int l = 0; l < KERNEL_SIZE; l++) {
-            sum += s_A(tx, l) * s_B(l, ty);
+        // copy tile of A and B to s_A and s_B
+        for (int i = 0; i < MICROKERNEL_SIZE; i++) {
+            s_A(tilex + i, tiley) = A_(tilex + i, tiley);
+            s_B(tilex + i, tiley) = B_(tilex + i, tiley);
         }
-        __syncthreads();  // this is vital!
+        __syncthreads();
+        
+        // compute partial sum in a 4x1 manner
+        for (int l = 0; l < KERNEL_SIZE; l++) {
+            sum0 += s_A(tilex + 0, l) * s_B(l, tiley);
+            sum1 += s_A(tilex + 1, l) * s_B(l, tiley);
+            sum2 += s_A(tilex + 2, l) * s_B(l, tiley);
+            sum3 += s_A(tilex + 3, l) * s_B(l, tiley);
+        }
+        // adjust A_ and B_
+        A_ = A_ + KERNEL_SIZE * M;
+        B_ = B_ + KERNEL_SIZE;
+        __syncthreads();
     }
-    C(i, j) = *alpha * sum + *beta * C(i, j);
+
+    float *C_ = C + blockIdx.x * KERNEL_SIZE + blockIdx.y * KERNEL_SIZE * M;
+    float alpha_ = *alpha;
+    float beta_ = *beta;
+    C_(tilex + 0, tiley) = alpha_ * sum0 + beta_ * C_(tilex + 0, tiley); 
+    C_(tilex + 1, tiley) = alpha_ * sum1 + beta_ * C_(tilex + 1, tiley); 
+    C_(tilex + 2, tiley) = alpha_ * sum2 + beta_ * C_(tilex + 2, tiley); 
+    C_(tilex + 3, tiley) = alpha_ * sum3 + beta_ * C_(tilex + 3, tiley); 
 
     return;
 
@@ -65,7 +79,7 @@ void sgemmKernel_v3(int M, int N, int K, const float *d_alpha, const float *d_be
     int div_M = DIV(M);
     int div_N = DIV(N);
     dim3 grid_size(div_M, div_N);
-    dim3 block_size(KERNEL_SIZE, KERNEL_SIZE);
+    dim3 block_size(KERNEL_SIZE/MICROKERNEL_SIZE, KERNEL_SIZE);
 
     kernelFunc<<<grid_size, block_size>>>(M, N, K, d_alpha, d_beta, d_A, d_B, d_C);
     
